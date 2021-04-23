b4895fcd2d07687716269b68b5d0fc584d85b433
Bonn <ryan.agharaad@gmail.com>
2021-04-23 01:34:47 -0400

    Practice for me

---
+++ kotlin_practice.kt
@@ -0,0 +1,7 @@
+fun main() {
+    //println makes a new line outputing a value 
+    //print does not make a new line
+    println("Just keep swimming")
+    println("Nice ")
+    println("Job!")
+}
---
+++ make_slarkbot_sad.py
@@ -0,0 +1,6 @@
+insults = ["you suck", "Fuck you", "stinky", "poopy head"]
+
+response = "So Mean"
+
+if insults == insults[0]:
+    print(response)
